41e017f76a7b527554f359dd630b3abfbf493c2d
mail@domain.com <kahlmeyer94@yahoo.de>
2023-08-24 11:01:38 +0200

    shorter version

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 """The setup script."""
 
-from setuptools import setup, find_packages
+from setuptools import setup
 
 with open('README.md') as readme_file:
     readme = readme_file.read()
@@ -17,29 +17,16 @@ requirements = [
     'tqdm'
 ]
 
-test_requirements = []
+
 
 setup(
+    name='DAG_search',
+    version=1.0,
+    description = "An open source python library symbolic regression based on searching the space of DAGs.",
+    long_description = readme,
     author="Paul Kahlmeyer",
     author_email='paul.kahlmeyer@uni-jena.de',
-    python_requires='=3.9.12',
-    classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Natural Language :: English',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-    ],
-
-    description="An open source python library symbolic regression based on searching the space of DAGs.",
-    install_requires=requirements,
-    long_description=readme,
-    include_package_data=True,
-    keywords='DAG_search',
-    name='DAG_search',
-    packages=find_packages(include=['DAG_search', 'DAG_search.*']),
-    url='https://github.com/kahlmeyer94/DAG_search'
+    url = 'https://github.com/kahlmeyer94/DAG_search',
+    packages = ['DAG_search'],
+    install_requires = requirements
 )
