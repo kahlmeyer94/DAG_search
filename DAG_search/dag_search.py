1de9a04f1dbc8e3786c4808ef145c79da5700d7e
mail@domain.com <kahlmeyer94@yahoo.de>
2023-08-25 14:25:03 +0200

    doubled default number of orders

--- DAG_search/dag_search.py
+++ DAG_search/dag_search.py
@@ -910,7 +910,7 @@ class DAGRegressor(sklearn.base.BaseEstimator, sklearn.base.RegressorMixin):
     Sklearn interface for exhaustive search.
     '''
 
-    def __init__(self, k:int = 1, n_calc_nodes:int = 4, max_orders:int = int(1e5), random_state:int = None, **kwargs):
+    def __init__(self, k:int = 1, n_calc_nodes:int = 4, max_orders:int = int(2e5), random_state:int = None, **kwargs):
 
         '''
         @Params:
