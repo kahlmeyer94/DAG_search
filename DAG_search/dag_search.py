a25fa8d69477e7dd183b7ade9df9cdc37a59e5dd
mail@domain.com <kahlmeyer94@yahoo.de>
2023-10-04 11:57:50 +0200

    fixed typo

--- DAG_search/dag_search.py
+++ DAG_search/dag_search.py
@@ -1149,7 +1149,7 @@ class DAGRegressor(sklearn.base.BaseEstimator, sklearn.base.RegressorMixin):
     Sklearn interface for exhaustive search.
     '''
 
-    def __init__(self, k:int = 1, n_calc_nodes:int = 4, max_orders:int = int(2e5), random_state:int = None, processes:int = None, max_samples:int = 100, mode : str = 'exhaustive', loss_fkt : DAG_Loss_fkt = MSE_loss_fkt **kwargs):
+    def __init__(self, k:int = 1, n_calc_nodes:int = 4, max_orders:int = int(2e5), random_state:int = None, processes:int = None, max_samples:int = 100, mode : str = 'exhaustive', loss_fkt : DAG_Loss_fkt = MSE_loss_fkt, **kwargs):
 
         '''
         @Params:
