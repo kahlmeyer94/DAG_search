a4f5cf2f1d5adbbf6425ddbcfd40096d5cbfdb09
mail@domain.com <kahlmeyer94@yahoo.de>
2023-12-15 08:12:46 +0100

    changed defult params

--- DAG_search/dag_search.py
+++ DAG_search/dag_search.py
@@ -1643,7 +1643,7 @@ class DAGRegressor(sklearn.base.BaseEstimator, sklearn.base.RegressorMixin):
     Sklearn interface for exhaustive search.
     '''
 
-    def __init__(self, k:int = 1, n_calc_nodes:int = 5, max_orders:int = int(2e5), random_state:int = None, processes:int = 1, max_samples:int = 100, stop_thresh:float = 1e-20, mode : str = 'exhaustive', loss_fkt :DAG_Loss_fkt = R2_loss_fkt, max_time:float = 800.0, positives:list = None, pareto:bool = False, results:dict = {}, **kwargs):
+    def __init__(self, k:int = 1, n_calc_nodes:int = 6, max_orders:int = int(5e5), random_state:int = None, processes:int = 1, max_samples:int = 100, stop_thresh:float = 1e-20, mode : str = 'exhaustive', loss_fkt :DAG_Loss_fkt = R2_loss_fkt, max_time:float = 1800.0, positives:list = None, pareto:bool = False, results:dict = {}, **kwargs):
         '''
         @Params:
             k.... number of constants
@@ -1913,7 +1913,7 @@ class DAGRegressorPoly(sklearn.base.BaseEstimator, sklearn.base.RegressorMixin):
     Sklearn interface for symbolic Regressor based on replacement strategies.
     '''
 
-    def __init__(self, random_state:int = None, simpl_nodes:int = 3, topk:int = 2, max_orders:int = int(1e5), max_degree:int = 3, max_tree_size:int = 30, max_samples:int = 100, processes:int = 1, **kwargs):
+    def __init__(self, random_state:int = None, simpl_nodes:int = 3, topk:int = 2, max_orders:int = int(1e5), max_degree:int = 3, max_tree_size:int = 30, max_samples:int = 1000, processes:int = 1, **kwargs):
         self.random_state = random_state
         self.processes = processes
         self.regr_search = self.regr_search = DAGRegressor(processes=self.processes, random_state = self.random_state)
