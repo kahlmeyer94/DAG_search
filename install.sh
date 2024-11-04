db4d16e0de79a56bb5a58da8f93c82fb60b6c53a
kahlmeyer94 <47181901+kahlmeyer94@users.noreply.github.com>
2024-11-04 08:16:10 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,12 @@
 #install dag search
 
 # remove directory if it exists
-if [ -d "UDFS" ]; then
-    rm -rf UDFS
+if [ -d "Unbiased-DAG-Frame-Search" ]; then
+    rm -rf Unbiased-DAG-Frame-Search
 fi
 
-git clone https://github.com/kahlmeyer94/UDFS
+git clone https://github.com/kahlmeyer94/Unbiased-DAG-Frame-Search
 
-cd UDFS
+cd Unbiased-DAG-Frame-Search
 pip install -r requirements.txt
 python setup.py install
